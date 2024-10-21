6eaedfd372cf815f7d491301593ebd14e965ce8b
momokonomi <momokonomipaint@gmail.com>
2024-10-21 13:53:05 +0000

    feat #1 install sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import './header.scss';
 import Header from '../components/header';
 
 const geistSans = localFont({
